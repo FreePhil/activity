8ea8acc53d04cd6296de545a4e4a2c62bd8edcd4
Phil@Djali <phil@ms1.hle.com.tw>
2018-11-27 12:12:14 +0800

    accomplish activity service

--- ActivityService/Services/IUserActivityService.cs
+++ ActivityService/Services/IUserActivityService.cs
@@ -1,7 +1,15 @@
+using System.Threading.Tasks;
+using ActivityService.Models;
+
 namespace ActivityService.Services
 {
     public interface IUserActivityService
     {
-        string GetPayload(string id);
+        Task<UserActivity> GetActivity(string id);
+        Task AddActivity(UserActivity activity);
+        
+        Task<bool> UpdatePayload(string id, string payload);
+        Task<bool> UpdateOption(string id, string option);
+        Task<bool> UpdateStatus(string id, string status);
     }
 }
--- ActivityService/Services/UserActivityService.cs
+++ ActivityService/Services/UserActivityService.cs
@@ -1,10 +1,40 @@
+using System.Threading.Tasks;
+using ActivityService.Models;
+using ActivityService.Repositories;
+
 namespace ActivityService.Services
 {
     public class UserActivityService: IUserActivityService
     {
-        public string GetPayload(string id)
+        private IRepository Repository { get; }
+        public UserActivityService(IRepository repository)
         {
-            throw new System.NotImplementedException();
+            Repository = repository;
+        }
+
+        public Task<UserActivity> GetActivity(string id)
+        {
+            return Repository.GetAsync(id);
+        }
+
+        public Task AddActivity(UserActivity activity)
+        {
+            return Repository.AddAsync(activity);
+        }
+
+        public Task<bool> UpdatePayload(string id, string payload)
+        {
+            return Repository.UpdateAsync(id, ac => ac.Payload, payload);
+        }
+
+        public Task<bool> UpdateOption(string id, string option)
+        {
+            return Repository.UpdateAsync(id, ac => ac.Option, option);
+        }
+
+        public Task<bool> UpdateStatus(string id, string status)
+        {
+            return Repository.UpdateAsync(id, ac => ac.Status, status);
         }
     }
 }
