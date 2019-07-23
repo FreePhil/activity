1fc8d555b1e929f9a89a7ffb7dbe7454581988d6
Phil@Djali <phil@ms1.hle.com.tw>
2019-07-23 15:28:43 +0800

    add log and health check

--- ActivityService/Controllers/SimpleLoginController.cs
+++ ActivityService/Controllers/SimpleLoginController.cs
@@ -17,6 +17,12 @@ namespace ActivityService.Controllers
         {
             Service = service;
         }
+
+        [HttpGet("/api/health")]
+        public ActionResult GetHealth()
+        {
+            return Ok();
+        }
         
         [HttpPost]
         public async Task<ActionResult<SimpleUser>> Login([FromBody] LoginUser login)
